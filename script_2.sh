dce92d9265e11920a2ec31cf4b5015159d2450ca
Ferry <akuemezuiferryjosnel@gmail.com>
2022-03-20 17:51:57 -0500

    New script in the feature1 file

---
+++ script_2.sh
@@ -0,0 +1,4 @@
+This is the second line of my code.
+This is more like an iteractive line of code where everyone can relate to something.
+I am happy that I am back at it again.
+Practice makes the difference.
